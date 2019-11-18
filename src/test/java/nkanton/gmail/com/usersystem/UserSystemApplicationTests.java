ba0759fd6b0467b00641ba5cdaf661bc614c52f4
nkanton <nkanton@gmail.com>
2019-11-18 13:31:22 +0200

    Initial commit. Spring boot setup.

---
+++ src/test/java/nkanton/gmail/com/usersystem/UserSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package nkanton.gmail.com.usersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
