9e3491790d467ec05348812f669c4e21897a324d
Anton Volchan <nkanton@gmail.com>
2019-11-18 23:00:53 +0200

    initializing angular project

--- src/main/java/nkanton/gmail/com/usersystem/controller/HomeController.java
+++ src/main/java/nkanton/gmail/com/usersystem/controller/HomeController.java
@@ -2,9 +2,15 @@ package nkanton.gmail.com.usersystem.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+    @GetMapping
+    public String home(Model model) {
+        return "forward:/index.html";
+    }
 }
