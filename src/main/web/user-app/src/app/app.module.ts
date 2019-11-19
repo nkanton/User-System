ac863d601cd0cd4abb0aec406f39f1734360692f
Anton Volchan <nkanton@gmail.com>
2019-11-19 10:52:08 +0200

    Basic structure info, create user. Navigation bar, header.

--- src/main/web/user-app/src/app/app.module.ts
+++ src/main/web/user-app/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FooterComponent} from "./footer/footer.component";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {SidebarModule} from "ng-sidebar";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    SidebarModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
